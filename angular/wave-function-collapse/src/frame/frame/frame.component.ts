55479d8757029c8c0d6c57c023ddcd5c1a78677e
DavoudTavakol <tavit666@gmail.com>
2023-02-14 16:57:26 +0100

    Setup for WFC
    Created basic tiles png's.
    Drawing Images multiple times on canvas.

--- angular/wave-function-collapse/src/frame/frame/frame.component.ts
+++ angular/wave-function-collapse/src/frame/frame/frame.component.ts
@@ -10,22 +10,48 @@ export class FrameComponent implements OnInit {
 
   ngOnInit(): void {
     const canvas: HTMLCanvasElement = this.myCanvas.nativeElement;
-    const ctx = canvas.getContext('2d');
+    const ctx = canvas.getContext('2d')!;
+
+    const DIM: number = 4;
+    const tileSize: number = 50;
+    const canvasWidth: number = DIM * tileSize;
+    const canvasHeight: number = DIM * tileSize;
+
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
 
     const tiles: any = [];
 
-    function preload() {
-      tiles[0] = new Image();
-      tiles[0].src = 'assets/images/Sign_white.png';
+    preload(whendone);
+
+    function whendone(tiles: any) {
+
+      ctx.drawImage(tiles[0], 0, 0);
+      ctx.drawImage(tiles[1], 0+tileSize, 0);
+      ctx.drawImage(tiles[2], 0+tileSize*2, 0);
+      ctx.drawImage(tiles[2], 0+tileSize*3, 0);
+/*
+      for (let i = 0; i < DIM; i++) {
+        for (let j = 0; j < DIM; j++) {
+          ctx.drawImage(tiles[1], 0 + i * tileSize, 0 + j * tileSize);
+        }
+      }
+*/
     }
 
-    if (ctx) {
-      preload();
+    function preload(callback: any) {
+      let total = 0;
+      let loaded = 0;
 
-      tiles[0].onload = () => {
-        ctx.drawImage(tiles[0], 0, 0);
-      };
-      
+      for (let i = 0; i < 4; i++) {
+        tiles[i] = new Image();
+        tiles[i].onload = function () {
+          if (++loaded == total)
+            callback (tiles);
+        };
+        tiles[i].src = 'assets/images/' + i + '.png';
+        total++;
+      }
     }
   }
 }
