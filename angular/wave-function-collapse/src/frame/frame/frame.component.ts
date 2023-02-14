09c0a02148c4a42c61f0b4f7c12aed44058dc7c4
DavoudTavakol <tavit666@gmail.com>
2023-02-14 17:38:05 +0100

    added grid

--- angular/wave-function-collapse/src/frame/frame/frame.component.ts
+++ angular/wave-function-collapse/src/frame/frame/frame.component.ts
@@ -12,7 +12,7 @@ export class FrameComponent implements OnInit {
     const canvas: HTMLCanvasElement = this.myCanvas.nativeElement;
     const ctx = canvas.getContext('2d')!;
 
-    const DIM: number = 2;
+    const DIM: number = 15;
     const tileSize: number = 50;
     const canvasWidth: number = DIM * tileSize;
     const canvasHeight: number = DIM * tileSize;
@@ -29,20 +29,22 @@ export class FrameComponent implements OnInit {
       'assets/images/3.png',
     ];
 
+    drawGrid();
     preload(srcUrl, whendone);
 
     function whendone(tiles: any) {
+      //test
       ctx.drawImage(tiles[0], 0, 0);
       ctx.drawImage(tiles[1], 0 + tileSize, 0);
       ctx.drawImage(tiles[2], 0, 0 + tileSize);
       ctx.drawImage(tiles[3], tileSize, tileSize);
-      /*
+
       for (let i = 0; i < DIM; i++) {
         for (let j = 0; j < DIM; j++) {
-          ctx.drawImage(tiles[1], 0 + i * tileSize, 0 + j * tileSize);
+          //test with random tile
+          //ctx.drawImage(tiles[Math.floor(Math.random() * 4)],i * tileSize,j * tileSize);
         }
       }
-*/
     }
 
     function preload(urls: string[], callback: any) {
@@ -57,7 +59,16 @@ export class FrameComponent implements OnInit {
           }
         };
         loaded++;
-       
+      }
+    }
+
+    function drawGrid() {
+      for (let i = 0; i < DIM; i++) {
+        for (let j = 0; j < DIM; j++) {
+          ctx.lineWidth = 1;
+          ctx.strokeStyle = 'rgba(255,255,255,0.1)';
+          ctx.strokeRect(i * tileSize, j * tileSize, tileSize, tileSize);
+        }
       }
     }
   }
