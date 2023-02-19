c5e4bc2777b7717c6d7f42a7a700359744d55567
DavoudTavakol <tavit666@gmail.com>
2023-02-19 15:18:03 +0100

    Collapsing and evaluation entropy

--- angular/wave-function-collapse/src/frame/frame/frame.component.ts
+++ angular/wave-function-collapse/src/frame/frame/frame.component.ts
@@ -18,18 +18,19 @@ export class FrameComponent implements OnInit {
     'assets/images/1.png',
     'assets/images/2.png',
     'assets/images/3.png',
+    'assets/images/4.png'
   ];
 
-  DIM: number = 2;
+  DIM: number = 20;
   tileSize: number = 50;
   canvasWidth: number = this.DIM * this.tileSize;
   canvasHeight: number = this.DIM * this.tileSize;
 
   grid: any = [];
   readonly BLANK: number = 0;
-  readonly UP: number = 0;
-  readonly RIGHT: number = 0;
-  readonly DOWN: number = 0;
+  readonly UP: number = 1;
+  readonly RIGHT: number = 2;
+  readonly DOWN: number = 3;
   readonly LEFT: number = 4;
 
   constructor() {}
@@ -45,7 +46,82 @@ export class FrameComponent implements OnInit {
     this.canvas.width = this.canvasWidth;
     this.canvas.height = this.canvasHeight;
     this.drawGrid(this.ctx);
-    this.preload(this.srcUrl, this.tiles); 
+    this.preload(this.srcUrl, this.tiles);
+
+    
+  }
+
+  //YOU CAN DRAW IMAGES HERE NOW SINCE ALL PRELOADED
+  init() {
+
+    console.dir('STARTING..');
+    //Filling Grid
+    for (let i = 0; i < this.DIM * this.DIM; i++) {
+      console.log("i: "+i);
+      this.grid[i] = {
+        collapsed: false,
+        options: [this.BLANK, this.UP, this.RIGHT, this.DOWN, this.LEFT]
+      };
+    }
+    
+    ////Pick cell/cells with least entropy (sort all the cells then pick. if all same => pick random)
+    //sort grid by # of options
+    const gridCopy = [...this.grid];
+    gridCopy.sort((a: any, b: any) => {
+      return a.options.length - b.options.length;
+    });
+   
+    //find index with all the lowest entropy cells
+    //gridcopy[0] because we sorted it and the first cell should habe the fewest options.
+    let lowestOptionNr = gridCopy[0].length;
+    let cellPick = 0;
+    for(let i = 1; i < lowestOptionNr; i++) { //i = 1 because we dont need to compare with himself at gridCopy[0] if i = 0 were used
+      if(gridCopy[i].option.length > lowestOptionNr){
+        cellPick = i;
+        break;
+      }
+    }
+
+    //remove all other cells
+    if(lowestOptionNr > 0) gridCopy.splice(cellPick);
+    //pick random cell
+    const cell = gridCopy[this.getRandomInt(gridCopy.length)];
+    //collapse cell
+    cell.collapsed = true; //why is this happening first and manipulation the base grid???
+    //pick random option
+    let pick = cell.options[this.getRandomInt(cell.options.length)];
+    //replace option in cell
+    cell.options = [pick];
+    //go through all of the grid and draw cell IF its collapsed!
+    for (let i = 0; i < this.DIM; i++) {
+      for (let j = 0; j < this.DIM; j++) {
+        let tempCell = this.grid[i + j * this.DIM];
+        if (tempCell.collapsed) {
+          let index = tempCell.options[0];
+          this.ctx.drawImage(
+            this.tiles[index],
+            i * this.tileSize,
+            j * this.tileSize
+          );
+        }
+      }
+    }
+    
+  }
+
+  x: number = 0;
+  y: number = 0;
+  onClick() {
+    this.ctx.drawImage(
+      this.tiles[Math.floor(Math.random() * 4)],
+      this.x,
+      this.y
+    );
+    this.x = this.x + this.tileSize;
+    if (this.x >= this.tileSize * this.DIM) {
+      this.y = this.y + this.tileSize;
+      this.x = 0;
+    }
   }
 
   preload(urls: string[], tiles: any) {
@@ -79,63 +155,7 @@ export class FrameComponent implements OnInit {
     }
   }
 
-
-
-
-  //YOU CAN DRAW IMAGES HERE NOW SINCE ALL PRELOADED
-  init() {
-    console.dir("STARTING..");
-
-    //Filling Grid
-    for (let i = 0; i < this.DIM * this.DIM; i++) {
-      this.grid[i] = {
-        collapsed: false,
-        options: [this.BLANK, this.UP, this.RIGHT, this.DOWN, this.LEFT],
-      };
-    }
-
-    this.grid[2].options = [this.BLANK, this.UP];
-
-
-    //Pick cell with least entropy (sort all the cells then pick. if all same => pick random)
-    const gridCopy = this.grid.slice();
-    gridCopy.sort((a: any, b: any) => {
-      return a.options.length - b.options.length;
-    });
-    console.log(this.grid);
-    console.log(gridCopy);
-
-
-    for (let i = 0; i < this.DIM; i++) {
-      for (let j = 0; j < this.DIM; j++) {
-        let cell = this.grid[i + j * this.DIM];
-        cell.collapsed = true;
-        if (cell.collapsed) {
-          let index = cell.options[0];
-          this.ctx.drawImage(
-            this.tiles[index],
-            i * this.tileSize,
-            j * this.tileSize
-          );
-        }
-      }
-    }
-  }
-
-  
-
-  x: number = 0;
-  y: number = 0;
-  onClick() {
-    this.ctx.drawImage(
-      this.tiles[Math.floor(Math.random() * 4)],
-      this.x,
-      this.y
-    );
-    this.x = this.x + this.tileSize;
-    if (this.x >= this.tileSize * this.DIM) {
-      this.y = this.y + this.tileSize;
-      this.x = 0;
-    }
+  getRandomInt(max: number): number {
+    return Math.floor(Math.random() * max);
   }
 }
